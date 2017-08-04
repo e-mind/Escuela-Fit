f8d6c95325b0a9142a29942ac3a6ba04cec8e904
Fer Gv <fkngv_10@hotmail.com>
2017-08-04 01:42:26 -0500

    Gráfica en index de app 'pase_lista'

--- apps/pase_lista/urls.py
+++ apps/pase_lista/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-# from django.views.generic import TemplateView
+from django.views.generic import TemplateView
 
-# from .views import json_chart
+from .views import json_chart
 
 urlpatterns = [
-#     url(r'^$', TemplateView.as_view(template_name="pase_lista/index.html"), name='index'),
-#     url(r'^json_data/$', json_chart, name='json_data'),
+    url(r'^$', TemplateView.as_view(template_name="pase_lista/index.html"), name='index'),
+    url(r'^json_data/$', json_chart, name='json_data'),
 ]
--- apps/pase_lista/views.py
+++ apps/pase_lista/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
-# Create your views here.
+def json_chart(request):
+    json_data = {
+        'labels': ["January", "February", "March", "April", "May", "June", "July"],
+        'datasets': [{
+            'label': "My First dataset",
+            'backgroundColor': 'rgba(255, 99, 132, 0.2)',
+            'borderColor': 'rgb(255, 99, 132)',
+            'data': [0, 10, 5, 2, 20, 30, 45],
+        }]
+    }
+    return JsonResponse(json_data)
