54be7a96679e1d13e2454416341d57b1b673aa06
Fer Gv <fkngv_10@hotmail.com>
2017-08-06 01:34:10 -0500

    Comunicación serial con arduino

--- apps/pase_lista/urls.py
+++ apps/pase_lista/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from .views import json_data
+from .views import json_data, asistencia
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name="pase_lista/index.html"), name='index'),
     url(r'^json_data/$', json_data, name='json_data'),
+    url(r'^asistencia/$', asistencia, name='asistencia'),
 ]
