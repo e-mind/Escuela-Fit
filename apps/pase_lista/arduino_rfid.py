b5afd995071c04a6314d0eea8a6611111092ecb7
Fer Gv <fkngv_10@hotmail.com>
2017-08-11 10:40:59 -0500

    Ahora la petición para el registro de asistencia se hace por método POST

--- apps/pase_lista/arduino_rfid.py
+++ apps/pase_lista/arduino_rfid.py
@@ -1,17 +1,16 @@
-import serial
-from urllib.request import urlopen, pathname2url
+import serial, requests
 
 serial_conection = serial.Serial("COM4", 9600)
-url_origin = 'http://127.0.0.1:8000/pase_lista/asistencia/{}/'
+url = 'http://127.0.0.1:8000/pase_lista/asistencia/'
 
 # while True:
 try:
     rfid_bytes = serial_conection.readline()
     if rfid_bytes:
         rfid_string = rfid_bytes[1:-2].decode('utf-8')
-        url_arguments = url_origin.format(pathname2url(rfid_string))
-        data = urlopen(url_arguments).read().decode('utf-8')
-        print(data)
+        data = requests.post(url, data={'rfid': rfid_string})
 except:
     serial_conection.close()
     exit()
+else:
+    serial_conection.close()
--- apps/pase_lista/views.py
+++ apps/pase_lista/views.py
@@ -1,11 +1,14 @@
 # from django.shortcuts import render, redirect
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def json_data(request):
     data_response = [5, 10, 20, 30, 20, 10, 5]
     return JsonResponse(data_response, safe=False)
 
-def asistencia(request, rfid=None):
+@csrf_exempt
+def asistencia(request):
+    rfid = request.POST['rfid']
     print(rfid)
     return HttpResponse(rfid)
