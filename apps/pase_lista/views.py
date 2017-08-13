a2651984b51b792a443566b4ca2b115f5e18f051
Fer Gv <fkngv_10@hotmail.com>
2017-08-13 00:12:35 -0500

    Registro de asistencia a través de la petición POST

--- apps/pase_lista/views.py
+++ apps/pase_lista/views.py
@@ -2,6 +2,9 @@
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from .models import Asistencia
+from apps.alumnos.models import AlumnoDatosPersonales
+
 
 def json_data(request):
     data_response = [5, 10, 20, 30, 20, 10, 5]
@@ -10,5 +13,10 @@ def json_data(request):
 @csrf_exempt
 def asistencia(request):
     rfid = request.POST['rfid']
-    print(rfid)
+    alumno = AlumnoDatosPersonales.objects.get(codigo_tarjeta=rfid)
+    registro = alumno.asistencia_set.create()
+    print(registro.id)
+    print(registro.fecha)
+    print(registro.hora)
+    print(registro.alumno)
     return HttpResponse(rfid)
