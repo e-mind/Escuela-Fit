168aa05f73f64d28567371be45223c60b0d1e2db
Fer Gv <fkngv_10@hotmail.com>
2017-09-25 13:43:44 -0500

    1. Se agregan templates y archivos staticos   2. Se modifican url's

--- Escuela_Fit/urls.py
+++ Escuela_Fit/urls.py
@@ -1,9 +1,18 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from .views import index
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', index, name='index'),
+	# MAIN
+    url(r'^$', TemplateView.as_view(template_name='main/index.html'), name='index'),
+    url(r'^login/$', TemplateView.as_view(template_name='main/iniciars.html'), name='login'),
+    # url(r'^logout/$', , name='logout'),
+    url(r'^rutina/$', TemplateView.as_view(template_name='main/rutina.html'), name='rutina'),
+    url(r'^dieta/$', TemplateView.as_view(template_name='main/nutricion.html'), name='dieta'),
+    url(r'^calidad_aire/$', TemplateView.as_view(template_name='main/ca.html'), name='calidad_aire'),
+    url(r'^perfil/$', TemplateView.as_view(template_name='main/perfil.html'), name='perfil'),
+
+    # APPS
     url(r'^admin/', admin.site.urls),
     url(r'^pase_lista/', include('apps.pase_lista.urls', namespace='pase_lista')),
     url(r'^alumnos/', include('apps.alumnos.urls', namespace='alumnos')),
